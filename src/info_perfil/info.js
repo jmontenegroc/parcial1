ad4b9d54dc5069ab30d01478e5f6a1986563f879
Juan Montenegro <j.montenegroc@uniandes.edu.co>
2024-03-15 23:33:14 -0500

    update json

--- src/info_perfil/info.js
+++ src/info_perfil/info.js
@@ -1,12 +1,22 @@
-import './info.css';
+import { Col, Container, Row } from "react-bootstrap";
+import "./info.css";
 
-const Info = (props) =>{
-    return(
-        <div>
+const Info = (props) => {
+  return (
+    <div>
+      <Container>
+        <Row id="general">
+          <Col xs={5}>
             <img src={props.perfil.pfp} alt="pfp"></img>
-            {props.perfil.usuario}
-        </div>
-    )
-}
+          </Col>
+          <Col xs={5} id="text">
+            <h4>{props.perfil.usuario}</h4>
+            <p>{props.perfil.description}</p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
-export default Info
+export default Info;
